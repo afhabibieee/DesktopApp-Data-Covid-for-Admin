17547aa0cd9a016ef5e538adf3bdea488d01e727
Adil Faruq Habibi <70691510+afhabibieee@users.noreply.github.com>
2021-03-10 18:02:52 +0700

    Update FormTerkonfirmasi.vb
    Terkonfirmasi code that controls crud operation, which refers to the terkonfirmasi form

--- main-page/btn-master-data/FormTerkonfirmasi.vb
+++ main-page/btn-master-data/FormTerkonfirmasi.vb
@@ -1,232 +1,130 @@
-<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
-Partial Class FormTerkonfirmasi
-    Inherits System.Windows.Forms.Form
+Imports System.Data
+Imports System.Data.OleDb
 
-    'Form overrides dispose to clean up the component list.
-    <System.Diagnostics.DebuggerNonUserCode()> _
-    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
-        Try
-            If disposing AndAlso components IsNot Nothing Then
-                components.Dispose()
-            End If
-        Finally
-            MyBase.Dispose(disposing)
-        End Try
+Public Class FormTerkonfirmasi
+    Dim Myconection As New CLS.clsDatabaseConnection
+    Dim dtData As New Data.DataTable
+
+    Sub FormatGrid()
+        DataGridView1.Columns(0).Width = 100
+        DataGridView1.Columns(1).Width = 100
+        DataGridView1.Columns(2).Width = 100
+        DataGridView1.Columns(3).Width = 100
+        DataGridView1.Columns(4).Width = 100
+
+        DataGridView1.Columns(0).HeaderText = "RW"
+        DataGridView1.Columns(1).HeaderText = "Positif"
+        DataGridView1.Columns(2).HeaderText = "Pasien"
+        DataGridView1.Columns(3).HeaderText = "Sembuh"
+        DataGridView1.Columns(4).HeaderText = "Meninggal"
+
+        DataGridView1.ReadOnly = True
     End Sub
 
-    'Required by the Windows Form Designer
-    Private components As System.ComponentModel.IContainer
+    Sub Browse()
+        objdt.Clear()
+        strsql = ("Select rw, positif, pasien, sembuh, meninggal from [tbl_terkonfirmasi] where lcase(rw) like '%" & Trim(txtRW2.Text) & "%' order by positif;")
+        objcmd = New OleDbCommand With {
+            .Connection = Myconection.Open,
+            .CommandType = CommandType.Text,
+            .CommandText = strsql
+        }
+        objda = New OleDbDataAdapter(objcmd)
+        objda.Fill(objds, "tbl_terkonfirmasi")
+        objdt = objds.Tables("tbl_terkonfirmasi")
+        Me.DataGridView1.DataSource = objdt
+        FormatGrid()
+        Myconection.Close()
+        'tambahan buat edit
+        txtRW.DataBindings.Clear()
+        txtPositif.DataBindings.Clear()
+        txtPasien.DataBindings.Clear()
+        txtSembuh.DataBindings.Clear()
+        txtMeninggal.DataBindings.Clear()
 
-    'NOTE: The following procedure is required by the Windows Form Designer
-    'It can be modified using the Windows Form Designer.  
-    'Do not modify it using the code editor.
-    <System.Diagnostics.DebuggerStepThrough()> _
-    Private Sub InitializeComponent()
-        Me.Label6 = New System.Windows.Forms.Label()
-        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
-        Me.txtRW2 = New System.Windows.Forms.TextBox()
-        Me.Label5 = New System.Windows.Forms.Label()
-        Me.Label4 = New System.Windows.Forms.Label()
-        Me.Label3 = New System.Windows.Forms.Label()
-        Me.Label2 = New System.Windows.Forms.Label()
-        Me.Label1 = New System.Windows.Forms.Label()
-        Me.BtnEdit = New System.Windows.Forms.Button()
-        Me.BtnHapus = New System.Windows.Forms.Button()
-        Me.BtnSimpan = New System.Windows.Forms.Button()
-        Me.btnBrowse = New System.Windows.Forms.Button()
-        Me.txtMeninggal = New System.Windows.Forms.TextBox()
-        Me.txtSembuh = New System.Windows.Forms.TextBox()
-        Me.txtPasien = New System.Windows.Forms.TextBox()
-        Me.txtPositif = New System.Windows.Forms.TextBox()
-        Me.txtRW = New System.Windows.Forms.TextBox()
-        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
-        Me.SuspendLayout()
-        '
-        'Label6
-        '
-        Me.Label6.AutoSize = True
-        Me.Label6.Location = New System.Drawing.Point(56, 15)
-        Me.Label6.Name = "Label6"
-        Me.Label6.Size = New System.Drawing.Size(26, 13)
-        Me.Label6.TabIndex = 34
-        Me.Label6.Text = "RW"
-        '
-        'DataGridView1
-        '
-        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
-        Me.DataGridView1.Location = New System.Drawing.Point(14, 49)
-        Me.DataGridView1.Name = "DataGridView1"
-        Me.DataGridView1.Size = New System.Drawing.Size(558, 150)
-        Me.DataGridView1.TabIndex = 33
-        '
-        'txtRW2
-        '
-        Me.txtRW2.Location = New System.Drawing.Point(140, 12)
-        Me.txtRW2.Name = "txtRW2"
-        Me.txtRW2.Size = New System.Drawing.Size(100, 20)
-        Me.txtRW2.TabIndex = 32
-        '
-        'Label5
-        '
-        Me.Label5.AutoSize = True
-        Me.Label5.Location = New System.Drawing.Point(56, 320)
-        Me.Label5.Name = "Label5"
-        Me.Label5.Size = New System.Drawing.Size(56, 13)
-        Me.Label5.TabIndex = 31
-        Me.Label5.Text = "Meninggal"
-        '
-        'Label4
-        '
-        Me.Label4.AutoSize = True
-        Me.Label4.Location = New System.Drawing.Point(56, 294)
-        Me.Label4.Name = "Label4"
-        Me.Label4.Size = New System.Drawing.Size(46, 13)
-        Me.Label4.TabIndex = 30
-        Me.Label4.Text = "Sembuh"
-        '
-        'Label3
-        '
-        Me.Label3.AutoSize = True
-        Me.Label3.Location = New System.Drawing.Point(56, 268)
-        Me.Label3.Name = "Label3"
-        Me.Label3.Size = New System.Drawing.Size(39, 13)
-        Me.Label3.TabIndex = 29
-        Me.Label3.Text = "Pasien"
-        '
-        'Label2
-        '
-        Me.Label2.AutoSize = True
-        Me.Label2.Location = New System.Drawing.Point(56, 242)
-        Me.Label2.Name = "Label2"
-        Me.Label2.Size = New System.Drawing.Size(35, 13)
-        Me.Label2.TabIndex = 28
-        Me.Label2.Text = "Positif"
-        '
-        'Label1
-        '
-        Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(56, 216)
-        Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(26, 13)
-        Me.Label1.TabIndex = 27
-        Me.Label1.Text = "RW"
-        '
-        'BtnEdit
-        '
-        Me.BtnEdit.Location = New System.Drawing.Point(497, 342)
-        Me.BtnEdit.Name = "BtnEdit"
-        Me.BtnEdit.Size = New System.Drawing.Size(75, 23)
-        Me.BtnEdit.TabIndex = 26
-        Me.BtnEdit.Text = "Edit"
-        Me.BtnEdit.UseVisualStyleBackColor = True
-        '
-        'BtnHapus
-        '
-        Me.BtnHapus.Location = New System.Drawing.Point(416, 342)
-        Me.BtnHapus.Name = "BtnHapus"
-        Me.BtnHapus.Size = New System.Drawing.Size(75, 23)
-        Me.BtnHapus.TabIndex = 25
-        Me.BtnHapus.Text = "Hapus"
-        Me.BtnHapus.UseVisualStyleBackColor = True
-        '
-        'BtnSimpan
-        '
-        Me.BtnSimpan.Location = New System.Drawing.Point(335, 342)
-        Me.BtnSimpan.Name = "BtnSimpan"
-        Me.BtnSimpan.Size = New System.Drawing.Size(75, 23)
-        Me.BtnSimpan.TabIndex = 24
-        Me.BtnSimpan.Text = "Simpan"
-        Me.BtnSimpan.UseVisualStyleBackColor = True
-        '
-        'btnBrowse
-        '
-        Me.btnBrowse.Location = New System.Drawing.Point(246, 10)
-        Me.btnBrowse.Name = "btnBrowse"
-        Me.btnBrowse.Size = New System.Drawing.Size(75, 23)
-        Me.btnBrowse.TabIndex = 23
-        Me.btnBrowse.Text = "Browse"
-        Me.btnBrowse.UseVisualStyleBackColor = True
-        '
-        'txtMeninggal
-        '
-        Me.txtMeninggal.Location = New System.Drawing.Point(140, 317)
-        Me.txtMeninggal.Name = "txtMeninggal"
-        Me.txtMeninggal.Size = New System.Drawing.Size(100, 20)
-        Me.txtMeninggal.TabIndex = 22
-        '
-        'txtSembuh
-        '
-        Me.txtSembuh.Location = New System.Drawing.Point(140, 291)
-        Me.txtSembuh.Name = "txtSembuh"
-        Me.txtSembuh.Size = New System.Drawing.Size(100, 20)
-        Me.txtSembuh.TabIndex = 21
-        '
-        'txtPasien
-        '
-        Me.txtPasien.Location = New System.Drawing.Point(140, 265)
-        Me.txtPasien.Name = "txtPasien"
-        Me.txtPasien.Size = New System.Drawing.Size(100, 20)
-        Me.txtPasien.TabIndex = 20
-        '
-        'txtPositif
-        '
-        Me.txtPositif.Location = New System.Drawing.Point(140, 239)
-        Me.txtPositif.Name = "txtPositif"
-        Me.txtPositif.Size = New System.Drawing.Size(100, 20)
-        Me.txtPositif.TabIndex = 19
-        '
-        'txtRW
-        '
-        Me.txtRW.Location = New System.Drawing.Point(140, 213)
-        Me.txtRW.Name = "txtRW"
-        Me.txtRW.Size = New System.Drawing.Size(100, 20)
-        Me.txtRW.TabIndex = 18
-        '
-        'FormTerkonfirmasi
-        '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
-        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(584, 377)
-        Me.Controls.Add(Me.Label6)
-        Me.Controls.Add(Me.DataGridView1)
-        Me.Controls.Add(Me.txtRW2)
-        Me.Controls.Add(Me.Label5)
-        Me.Controls.Add(Me.Label4)
-        Me.Controls.Add(Me.Label3)
-        Me.Controls.Add(Me.Label2)
-        Me.Controls.Add(Me.Label1)
-        Me.Controls.Add(Me.BtnEdit)
-        Me.Controls.Add(Me.BtnHapus)
-        Me.Controls.Add(Me.BtnSimpan)
-        Me.Controls.Add(Me.btnBrowse)
-        Me.Controls.Add(Me.txtMeninggal)
-        Me.Controls.Add(Me.txtSembuh)
-        Me.Controls.Add(Me.txtPasien)
-        Me.Controls.Add(Me.txtPositif)
-        Me.Controls.Add(Me.txtRW)
-        Me.Name = "FormTerkonfirmasi"
-        Me.Text = "FormTerkontaminasi"
-        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
-        Me.ResumeLayout(False)
-        Me.PerformLayout()
-
+        If objdt.Rows.Count <> 0 Then
+            Me.txtRW.DataBindings.Add("text", objdt, "rw")
+            Me.txtPositif.DataBindings.Add("text", objdt, "positif")
+            Me.txtPasien.DataBindings.Add("text", objdt, "pasien")
+            Me.txtSembuh.DataBindings.Add("text", objdt, "sembuh")
+            Me.txtMeninggal.DataBindings.Add("text", objdt, "meninggal")
+        End If
     End Sub
 
-    Friend WithEvents Label6 As Label
-    Friend WithEvents DataGridView1 As DataGridView
-    Friend WithEvents txtRW2 As TextBox
-    Friend WithEvents Label5 As Label
-    Friend WithEvents Label4 As Label
-    Friend WithEvents Label3 As Label
-    Friend WithEvents Label2 As Label
-    Friend WithEvents Label1 As Label
-    Friend WithEvents BtnEdit As Button
-    Friend WithEvents BtnHapus As Button
-    Friend WithEvents BtnSimpan As Button
-    Friend WithEvents btnBrowse As Button
-    Friend WithEvents txtMeninggal As TextBox
-    Friend WithEvents txtSembuh As TextBox
-    Friend WithEvents txtPasien As TextBox
-    Friend WithEvents txtPositif As TextBox
-    Friend WithEvents txtRW As TextBox
+    Private Sub Hapus()
+        strsql = ("delete from tbl_terkonfirmasi where rw ='" & DataGridView1.CurrentRow.Cells(0).Value & "'")
+        objcmd = New OleDbCommand With {
+            .Connection = Myconection.Open,
+            .CommandType = CommandType.Text,
+            .CommandText = strsql
+        }
+        objda = New OleDbDataAdapter(objcmd)
+        objda.Fill(objds, "tbl_terkonfirmasi")
+        objcmd.ExecuteNonQuery()
+        Myconection.Close()
+        MsgBox("Data Berhasil dihapus", MsgBoxStyle.Information, "Delete Data")
+        Browse()
+    End Sub
+
+    Private Sub SetClear()
+        txtRW.Text = ""
+        txtPositif.Text = ""
+        txtPasien.Text = ""
+        txtSembuh.Text = ""
+        txtMeninggal.Text = ""
+    End Sub
+
+    Private Sub Simpan()
+        strsql = ("insert into tbl_terkonfirmasi (rw, positif, pasien, sembuh, meninggal) values ('" & Trim(txtRW.Text) & "','" & Trim(txtPositif.Text) & "','" & Trim(txtPasien.Text) & "','" & Trim(txtSembuh.Text) & "','" & Trim(txtMeninggal.Text) & "')")
+        objcmd = New OleDbCommand With {
+            .Connection = Myconection.Open,
+            .CommandType = CommandType.Text,
+            .CommandText = strsql
+        }
+        objda = New OleDbDataAdapter(objcmd)
+        objda.Fill(objds, "tbl_terkonfirmasi")
+        objda.InsertCommand = objcmd
+        Myconection.Close()
+        Browse()
+    End Sub
+
+    Private Sub btnBrowse_Click(sender As Object, e As EventArgs) Handles btnBrowse.Click
+        Browse()
+    End Sub
+
+    Private Sub BtnHapus_Click(sender As Object, e As EventArgs) Handles BtnHapus.Click
+        Hapus()
+    End Sub
+
+    Private Sub BtnSimpan_Click(sender As Object, e As EventArgs) Handles BtnSimpan.Click
+        If txtRW.Text = "" Then
+            MsgBox("RW Tidak Boleh Kosong...", MsgBoxStyle.Information, "Perhatian")
+            txtRW.Focus()
+        ElseIf txtPositif.Text = "" Then
+            MsgBox("Jumlah Positif Tidak Boleh Kosong...", MsgBoxStyle.Information, "Perhatian")
+            txtPositif.Focus()
+        ElseIf txtPasien.Text = "" Then
+            MsgBox("Jumlah Pasien Buku Tidak Boleh Kosong...", MsgBoxStyle.Information, "Perhatian")
+            txtPasien.Focus()
+        Else
+            Call Simpan()
+            Call SetClear()
+            MsgBox("Data Berhasil ditambahkan...", MsgBoxStyle.Information, "Inser Data")
+        End If
+    End Sub
+
+    Private Sub BtnEdit_Click(sender As Object, e As EventArgs) Handles BtnEdit.Click
+        strsql = ("update tbl_terkonfirmasi set positif='" & Trim(txtPositif.Text) & "',pasien='" & Trim(txtPasien.Text) & "',sembuh='" & Trim(txtSembuh.Text) & "',meninggal='" & Trim(txtMeninggal.Text) & "' where [rw] ='" & txtRW.Text & "'")
+        objcmd = New OleDbCommand With {
+            .Connection = Myconection.Open,
+            .CommandType = CommandType.Text,
+            .CommandText = strsql
+        }
+        objda = New OleDbDataAdapter(objcmd)
+        objda.Fill(objds, "tbl_terkonfirmasi")
+        objcmd.ExecuteNonQuery()
+        Myconection.Close()
+        MsgBox("Edit Data Terkonfirmasi Sukses...!!!", MsgBoxStyle.Information, "Edit Data")
+        Browse()
+    End Sub
 End Class
